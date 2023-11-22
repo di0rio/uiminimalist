30113f9e873066d7e8568c936c8cade5d0cf2747
di0rio <caua.diorio29@gmail.com>
2023-11-22 16:09:17 -0300

    nav bar finalizada e container inicializado

--- Client/src/components/Box/Box.jsx
+++ Client/src/components/Box/Box.jsx
@@ -1,9 +1,30 @@
-import style from './Box.module.css'
+import style from "./Box.module.css";
+
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
 
 const Box = () => {
-  return (
-    <div>Box</div>
-  )
-}
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
 
-export default Box
+  return (
+    <div>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+      >
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </Stack>
+    </div>
+  );
+};
+
+export default Box;
--- Client/src/routes/Home/Home.jsx
+++ Client/src/routes/Home/Home.jsx
@@ -1,10 +1,17 @@
-import style from './Home.module.css'
+import Box from "../../components/Box/Box";
+import style from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div></div>
-  )
-}
+    <div className={style.main}>
+      <div className={style.title}>
+        <h1>Hi, Welcome back 👋</h1>
+      </div>
+      <div>
+        <Box></Box>
+      </div>
+    </div>
+  );
+};
 
-export default Home
-
+export default Home;
